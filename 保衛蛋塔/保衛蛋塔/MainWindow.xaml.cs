3baf8c93b3264ef78fd59df0f7846906b3b06ecc
kevinhsu95034 <kevinhsu95034@gmail.com>
2015-06-11 19:09:58 +0800

    123

---
+++ 保衛蛋塔/保衛蛋塔/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 保衛蛋塔
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpgradeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new Window1().ShowDialog();
+        }
+    }
+}
