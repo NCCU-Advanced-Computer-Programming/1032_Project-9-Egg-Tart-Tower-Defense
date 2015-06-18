b85272e0f93f07dcb9c9ba2e22a5003c7028e4c3
kevinhsu95034 <kevinhsu95034@gmail.com>
2015-06-18 18:46:39 +0800

    enemy

--- 保衛蛋塔/保衛蛋塔/MainWindow.xaml.cs
+++ 保衛蛋塔/保衛蛋塔/MainWindow.xaml.cs
@@ -32,8 +32,9 @@ namespace 保衛蛋塔
 
         private void Food1Btn_Click(object sender, RoutedEventArgs e)
         {
-            Enemy qqq = new Enemy(100, 1, 2);
-            qqq.Show(80,60,"enemy.png");
+            Enemy qqq = new Enemy(100, 1, 1);
+            var img = qqq.Show(200, 200, "/Images/enemy.png");
+            EnemyGrid.Children.Add(img);
         }
 
     }
